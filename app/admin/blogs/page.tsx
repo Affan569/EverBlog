2baa4e7901ed13c767c9cf37cd639ad5543071ba
Muhammad Affan Hussain Siddiqui <affanzahid612@gmail.com>
2026-08-16 18:15:12 +0500

    Fix import statement for next/link in page.tsx

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Link from next/link
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { getAllPosts, deletePost, togglePostStatus } from '@/lib/firebase-helpers'
